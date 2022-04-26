a11172cd85f47c4c83794a4e82d263eaf3ca099a
Lucas Costa <101674510+lcscostadev@users.noreply.github.com>
2022-04-25 21:30:19 -0300

    test

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector('.hamburger')
-const navbar = document.querySelector('.navbar')
+const navbar = document.querySelector('.navbar');
+const toTop = document.querySelector('.to-top');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
@@ -10,3 +11,11 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
     hamburger.classList.remove('active');
     navbar.classList.remove('active');
 }))
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        toTop.classList.add('active');
+    } else {
+        toTop.classList.remove('active');
+    }
+})
